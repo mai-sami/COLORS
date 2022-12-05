2ab69ebf4dbb727b777dbce3637a03f5eec1c9ff
mai-sami <mai.jaber@hotmail.com>
2022-12-05 20:28:58 +0200

    fixed format

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const boxColor = document.getElementById("box_color");
 // change text in box
 boxColor.textContent = "PANT ME"
 const btn = document.querySelector("button");
- 
+
 const input = document.querySelector(".content .controls input");
 
-// //items is 16  #xxxxxx 
+ //items is 16  #xxxxxx 
 const hexaCodeAllCharacters = [
   "0",
   "1",
@@ -29,32 +29,32 @@ const hexaCodeAllCharacters = [
 
 // make Fun
 const generateHexaCode = (hexaCode) => {
-    // if hexacode is get a (parameter)
-    if (hexaCode) {
+  // if hexacode is get a (parameter)
+  if (hexaCode) {
     // setting box background color
     box.style.backgroundColor = hexaCode;
-        // set background to box  
-        console.log(hexaCode, "hexaCode...........")
+    // set background to box  
+    console.log(hexaCode, "hexaCode...........")
     return;
   }
 
-    // start random color
-    let randomStarts = "#";
+  // start random color
+  let randomStarts = "#";
 
-    // make loop (number it 6 digit) and random number to get 16 color =>start from 0 to 15
-    for (let i = 0; i < 6; i++) {
-        //random  between 0 to 15
-        let randomNum = Math.round(Math.random() * 15);
-        // get the HexaCode element for the random Number
+  // make loop (number it 6 digit) and random number to get 16 color =>start from 0 to 15
+  for (let i = 0; i < 6; i++) {
+    //random  between 0 to 15
+    let randomNum = Math.round(Math.random() * 15);
+    // get the HexaCode element for the random Number
     let HexaCodes = hexaCodeAllCharacters[randomNum];
-        // add the HexaCode to a randomStarts  
-        randomStarts = randomStarts.concat(HexaCodes);
+    // add the HexaCode to a randomStarts  
+    randomStarts = randomStarts.concat(HexaCodes);
   }
 
-    //set background color to box
-    box.style.backgroundColor = randomStarts;
-    // set background value to text
-   document.getElementById("input__color").value = randomStarts
+  //set background color to box
+  box.style.backgroundColor = randomStarts;
+  // set background value to text
+  document.getElementById("input__color").value = randomStarts
 };
 // call Fun
 generateHexaCode();
@@ -62,23 +62,23 @@ generateHexaCode();
 // add clink to button
 btn.addEventListener("click", () => generateHexaCode());
 
- 
+
 input.addEventListener("keyup", (e) => {
   const value = e.target.value;
-  console.log(value,"value here!!!!!!!!!!!")
- 
+  console.log(value, "value here!!!!!!!!!!!")
+
   //  return nothing if for this conditions
-  if (!(value.length == 7 && value[0] === "#")) return 
-  
+  if (!(value.length == 7 && value[0] === "#")) return
+
   alert("the value is not valid"); //to check value input;
-    // using the reduce to check value, if the characters in hexaCodeCharacters array
-    const array = value
+  // using the reduce to check value, if the characters in hexaCodeCharacters array
+  const array = value
     .slice(1)
     .split("")
     .reduce((acc, chr) => {
-       if (acc) {
-                // inclues the char
-         return hexaCodeAllCharacters.includes(chr);
+      if (acc) {
+        // inclues the char
+        return hexaCodeAllCharacters.includes(chr);
       } else return false;
     }, true);
   // run the generateHexaCode function  use the input value
